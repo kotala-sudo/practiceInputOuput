9f3eb6f5ec0465495de0089a3376555f92adc909
Pratap Kotala <paul@yahoo.coms>
2021-03-21 20:30:15 -0500

    initial commit

---
+++ src/app/childcomponent/childcomponent.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-childcomponent',
+  templateUrl: './childcomponent.component.html',
+  styleUrls: ['./childcomponent.component.css']
+})
+export class ChildcomponentComponent implements OnInit {
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+}
